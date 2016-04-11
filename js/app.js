72b4dc3288e2db2745bfe95f202aceccb46459c2
gleung93 <gleung93@gmail.com>
2016-04-10 20:09:45 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,4 @@
+new WOW().init();
+
+$("#bugin").delay(1000).queue(function(){$("#bugin").attr("src", "images/bugin.gif")});
+$("#pattern1").delay(4000).queue(function(){$("#pattern1").attr("src", "images/testPattern.gif")});
